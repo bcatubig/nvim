6841a261fc75701460d3033331c8df3a9320c6ea
Brandon Catubig <pcatbran@twitch.tv>
2023-11-27 12:52:55 -0800

    add venv-selector

--- lua/plugins/langs/python.lua
+++ lua/plugins/langs/python.lua
@@ -97,4 +97,20 @@ return {
       })
     end,
   },
+  {
+    'linux-cultist/venv-selector.nvim',
+    cmd = 'VenvSelect',
+    opts = function(_, opts)
+      opts.dap_enabled = true
+      return vim.tbl_deep_extend('force', opts, {
+        name = {
+          'venv',
+          '.venv',
+          'env',
+          '.env',
+        },
+      })
+    end,
+    keys = { { '<leader>cv', '<cmd>:VenvSelect<cr>', desc = 'Select VirtualEnv' } },
+  },
 }
