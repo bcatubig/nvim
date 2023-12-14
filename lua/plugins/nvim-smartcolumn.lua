a44c8f10d8c1de3deee524a0297eb041711fd207
Brandon Catubig <bcatubig@pm.me>
2023-12-14 11:00:20 -0800

    add smartcolumn

---
+++ lua/plugins/nvim-smartcolumn.lua
@@ -0,0 +1,19 @@
+return {
+  'm4xshen/smartcolumn.nvim',
+  opts = {
+    colorcolumn = { 88 },
+    disabled_filetypes = {
+      'help',
+      'alpha',
+      'dashboard',
+      'neo-tree',
+      'Trouble',
+      'trouble',
+      'lazy',
+      'mason',
+      'notify',
+      'toggleterm',
+      'lazyterm',
+    },
+  },
+}
