60cfa895268b2d24892a92240597506160033c3c
Brandon Catubig <bcatubig@pm.me>
2023-12-18 12:44:18 -0800

    refactor nvim-cmp with tjdevries settings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     'L3MON4D3/LuaSnip',
     dependencies = {
@@ -13,31 +12,31 @@ return {
       history = true,
       delete_check_events = 'TextChanged',
     },
-    keys = {
-      {
-        '<tab>',
-        function()
-          return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
-        end,
-        expr = true,
-        silent = true,
-        mode = 'i',
-      },
-      {
-        '<tab>',
-        function()
-          require('luasnip').jump(1)
-        end,
-        mode = 's',
-      },
-      {
-        '<s-tab>',
-        function()
-          require('luasnip').jump(-1)
-        end,
-        mode = { 'i', 's' },
-      },
-    },
+    -- keys = {
+    --   {
+    --     '<tab>',
+    --     function()
+    --       return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
+    --     end,
+    --     expr = true,
+    --     silent = true,
+    --     mode = 'i',
+    --   },
+    --   {
+    --     '<tab>',
+    --     function()
+    --       require('luasnip').jump(1)
+    --     end,
+    --     mode = 's',
+    --   },
+    --   {
+    --     '<s-tab>',
+    --     function()
+    --       require('luasnip').jump(-1)
+    --     end,
+    --     mode = { 'i', 's' },
+    --   },
+    -- },
   },
   {
     'hrsh7th/nvim-cmp',
@@ -45,18 +44,18 @@ return {
     event = 'InsertEnter',
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
       'saadparwaiz1/cmp_luasnip',
       'onsails/lspkind.nvim',
     },
     opts = function()
-      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-
+      -- https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/completion.lua
       local cmp = require 'cmp'
       local lspkind = require 'lspkind'
-      local luasnip = require 'luasnip'
 
-      local M = {
+      return {
         snippet = {
           expand = function(args)
             require('luasnip').lsp_expand(args.body)
@@ -70,6 +69,7 @@ return {
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-e>'] = cmp.mapping.abort(),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
@@ -78,8 +78,6 @@ return {
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -87,25 +85,73 @@ return {
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
           end, { 'i', 's' }),
         },
 
-        sources = cmp.config.sources {
+        sources = cmp.config.sources({
+          { name = 'nvim_lua' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+        }, {
           { name = 'path' },
+          { name = 'buffer', keyword_length = 5 },
+        }),
+
+        sorting = {
+          -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- copied from cmp-under, but I don't think I need the plugin for this.
+            -- I might add some more of my own.
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find '^_+'
+              local _, entry2_under = entry2.completion_item.label:find '^_+'
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
         },
         formatting = {
-          format = lspkind.cmp_format(),
+          -- Youtube: How to set up nice formatting for your sources.
+          format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+              buffer = '[buf]',
+              nvim_lsp = '[LSP]',
+              nvim_lua = '[api]',
+              path = '[path]',
+              luasnip = '[snip]',
+              gh_issues = '[issues]',
+              tn = '[TabNine]',
+              eruby = '[erb]',
+            },
+          },
+        },
+
+        experimental = {
+          -- I like the new menu better! Nice work hrsh7th
+          native_menu = false,
+
+          -- Let's play with this for a day or two
+          ghost_text = false,
         },
       }
-
-      return M
     end,
     config = function(_, opts)
       require('cmp').setup(opts)
