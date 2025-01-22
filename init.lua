5769d5c922969edbd4dc02fad59b12759907cf3a
Brandon Catubig <bcatubig@pm.me>
2025-01-22 10:33:23 -0800

    init commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require('config.opts')
+require('config.keymaps')
+require('config.autocmds')
+require('config.lazy')
---
+++ lua/config/opts.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.mouse = 'a'
+opt.showmode = false
+opt.tabstop = 2
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.signcolumn = 'yes'
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+-- opt.list = true
+--opt.listchars = { tab = ' ▎', trail = '·', nbsp = '␣' }
+opt.inccommand = 'split'
+opt.cursorline = true
+opt.scrolloff = 10
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
