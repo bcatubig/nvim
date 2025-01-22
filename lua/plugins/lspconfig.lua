714340d23856d5afaebb9d18ce6adc9db5476bec
Brandon Catubig <bcatubig@pm.me>
2025-01-22 14:01:17 -0800

    add lsps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -151,14 +151,14 @@ return {
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -189,8 +189,11 @@ return {
             },
           },
         },
+        html = {},
+        cssls = {},
+        tailwindcss = {},
+        emmet_ls = {},
         pyright = {},
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -224,6 +227,9 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'goimports',
+        'isort',
+        'black',
+        'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
