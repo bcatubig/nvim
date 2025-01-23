9cc2cd2e659647aa79851c4f67c13c8507924092
Brandon Catubig <bcatubig@pm.me>
2025-01-23 11:33:09 -0800

    add session

---
+++ lua/plugins/session.lua
@@ -0,0 +1,29 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  },
+  keys = {
+    {
+      '<leader>wr',
+      '<cmd>SessionSearch<CR>',
+      desc = 'Session Search',
+    },
+    {
+      '<leader>ws',
+      '<cmd>SessionSave<CR>',
+      desc = 'Save Session',
+    },
+    {
+      '<leader>wa',
+      '<cmd>SessionToggleAutoSave<CR>',
+      desc = 'Toggle autosave',
+    },
+  },
+}
