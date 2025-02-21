5a4315d9761ba3e8bcbae6ee0f66715af2183558
Brandon Catubig <bcatubig@pm.me>
2025-02-21 13:54:19 -0800

    disable markdown lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         sls = { 'saltlint' },
         dockerfile = { 'hadolint' },
         terraform = { 'tflint' },
