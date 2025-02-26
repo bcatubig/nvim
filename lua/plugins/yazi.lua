fa81c8afad2665b8573bc1ea764bf050a4a2e55c
Brandon Catubig <bcatubig@pm.me>
2025-02-26 13:08:46 -0800

    use yazi instead of snacks

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      '<leader>-',
+      '<leader>e',
       mode = { 'n', 'v' },
       '<cmd>Yazi<cr>',
       desc = 'Open yazi at the current file',
