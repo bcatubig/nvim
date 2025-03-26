9e24733a8e929ba46a83b2a30b69a0b1c169747e
Brandon Catubig <bcatubig@pm.me>
2025-03-26 13:13:08 -0700

    update gutter signs

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -484,7 +484,7 @@ now(function()
     severity_sort = true,
     float = { border = "rounded", source = "if_many" },
     underline = { severity = vim.diagnostic.severity.ERROR },
-    signs = vim.g.have_nerd_font and {
+    signs = {
       text = {
         [vim.diagnostic.severity.ERROR] = "󰅚 ",
         [vim.diagnostic.severity.WARN] = "󰀪 ",
