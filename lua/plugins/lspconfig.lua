e37a069b7feb57f6564cfae6678612c3f7912682
Brandon Catubig <bcatubig@pm.me>
2025-04-15 11:36:25 -0700

    fix typoo

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -28,7 +28,7 @@ return {
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          man("gRn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("gRn", vim.lsp.buf.rename, "[R]e[n]ame")
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
