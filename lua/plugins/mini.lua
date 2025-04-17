8aab104bab28a8532775c754be5b3b62a55451b9
Brandon Catubig <bcatubig@pm.me>
2025-04-17 08:31:22 -0700

    remove mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,28 +9,11 @@ return {
         move_with_alt = true,
       },
     })
-    -- require("mini.starter").setup({
-    --   header = "who dares wins",
-    --   query_updaters = "abcdefghijklmnopqrstuvwxyz0123456789_.",
-    -- })
     require("mini.ai").setup({ n_lines = 500 })
     require("mini.comment").setup()
     require("mini.surround").setup()
-    require("mini.pairs").setup()
     require("mini.move").setup()
     require("mini.bufremove").setup()
     require("mini.bracketed").setup()
-    require("mini.git").setup()
-
-    require("mini.diff").setup({
-      view = {
-        style = "sign",
-        signs = {
-          add = "▎",
-          change = "▎",
-          delete = "",
-        },
-      },
-    })
   end,
 }
