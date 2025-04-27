29e09cc86cea11e39b7805c177e3b0b07823f353
Brandon Catubig <bcatubig@pm.me>
2025-04-26 21:47:55 -0700

    spice up fzf-lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,9 +3,11 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local fzf = require("fzf-lua")
+    local ignore = { "^node_modules/", "^.git/", "^.terraform/" }
 
     fzf.setup({
       { "hide" },
+      file_ignore_patterns = ignore,
       defaults = {
         formatter = "path.dirname_first",
       },
@@ -28,18 +30,66 @@ return {
         glob_separator = "%s%-%-",
       },
     })
+    fzf.register_ui_select()
   end,
   keys = {
     { "<leader>sh", "<cmd>FzfLua helptags<cr>", desc = "[S]earch [H]elp" },
     { "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "[S]earch [K]eymaps" },
-    { "<leader>sf", "<cmd>FzfLua files<cr>", desc = "[S]earch [F]iles" },
+    {
+      "<leader>sf",
+      function()
+        require("fzf-lua").files({
+          git_icons = false,
+          file_icons = false,
+          no_headers_i = true,
+          winopts = {
+            preview = { hidden = "hidden" },
+          },
+        })
+      end,
+      desc = "[S]earch [F]iles",
+    },
     { "<leader>sg", "<cmd>FzfLua live_grep_glob<cr>", desc = "[S]earch [G]rep" },
     { "<leader>sr", "<cmd>FzfLua live_grep_resume<cr>", desc = "[S]earch Resume" },
-    { "<leader>s.", "<cmd>FzfLua oldfiles<cr>", desc = "[S]earch Recent Files" },
+    {
+      "<leader>s.",
+      function()
+        require("fzf-lua").oldfiles({
+          file_icons = false,
+          no_header_i = true,
+          winopts = {
+            preview = { hidden = "hidden" },
+          },
+        })
+      end,
+      desc = "[S]earch Recent Files",
+    },
     { "<leader>sw", "<cmd>FzfLua grep_cWORD<cr>", desc = "[S]earch [W]ord" },
     { "<leader>sC", "<cmd>FzfLua colorschemes<cr>", desc = "[S]earch [C]olorschemes" },
     { "<leader>sc", "<cmd>FzfLua commands<cr>", desc = "[S]earch [C]ommands" },
-    { "<leader><leader>", "<cmd>FzfLua buffers<cr>", desc = "[S]earch Buffers" },
+    {
+      "<leader><leader>",
+      function()
+        require("fzf-lua").buffers({
+          git_icons = false,
+          file_icons = false,
+          no_header_i = true,
+          winopts = {
+            preview = { hidden = "hidden" },
+          },
+        })
+      end,
+      desc = "[S]earch Buffers",
+    },
     { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search current buffer" },
+    {
+      "<leader>gb",
+      function()
+        require("fzf-lua").git_branches({
+          cmd = "git branch --color",
+        })
+      end,
+      desc = "Change [G]it branch",
+    },
   },
 }
