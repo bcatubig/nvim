b0fbc88bbf8bc37ad6c7bb16a6ce80273eaa7665
Brandon Catubig <bcatubig@pm.me>
2025-05-02 08:32:51 -0700

    refactor config

--- init.lua
+++ init.lua
@@ -2,4 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.have_nerd_font = true
 
-require("config")
+require("config.lazy")
+require("config.opts")
+require("config.keymaps")
+require("config.filetypes")
